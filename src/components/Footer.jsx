b39374cbc02f202dbec2e732129e518e7cff97b9
Shiielty <56679623+Shiielty@users.noreply.github.com>
2023-06-05 02:17:09 +0700

    Create Footer and Shop page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Copyright © 2023 |{" "}
+        <a href="https://github.com/Shiielty" target="_blank" rel="noreferrer">
+          Shiielty <i className="fa-brands fa-github"></i>
+        </a>
+      </p>
+    </footer>
+  );
+}
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,3 +1,83 @@
+import Footer from "../components/Footer";
+import "./Shop.css";
+
 export default function Shop() {
-  return <div>Shop</div>;
+  return (
+    <div className="shop">
+      <section className="shop-header">
+        <h2>Our Products</h2>
+        <p>
+          Made with <i className="fa-solid fa-heart"></i>
+        </p>
+        <div className="bg-tint"></div>
+      </section>
+      <section className="product-wrapper">
+        <div className="product">
+          <img src="" alt="" className="product-img" />
+          <div className="product-info">
+            <p className="product-name">Black coffee</p>
+            <p className="product-price">3$</p>
+            <button type="button">Add to cart</button>
+          </div>
+        </div>
+        <div className="product">
+          <img src="" alt="" className="product-img" />
+          <div className="product-info">
+            <p className="product-name">Black coffee</p>
+            <p className="product-price">3$</p>
+            <button type="button">Add to cart</button>
+          </div>
+        </div>
+        <div className="product">
+          <img src="" alt="" className="product-img" />
+          <div className="product-info">
+            <p className="product-name">Black coffee</p>
+            <p className="product-price">3$</p>
+            <button type="button">Add to cart</button>
+          </div>
+        </div>
+        <div className="product">
+          <img src="" alt="" className="product-img" />
+          <div className="product-info">
+            <p className="product-name">Black coffee</p>
+            <p className="product-price">3$</p>
+            <button type="button">Add to cart</button>
+          </div>
+        </div>
+        <div className="product">
+          <img src="" alt="" className="product-img" />
+          <div className="product-info">
+            <p className="product-name">Black coffee</p>
+            <p className="product-price">3$</p>
+            <button type="button">Add to cart</button>
+          </div>
+        </div>
+        <div className="product">
+          <img src="" alt="" className="product-img" />
+          <div className="product-info">
+            <p className="product-name">Black coffee</p>
+            <p className="product-price">3$</p>
+            <button type="button">Add to cart</button>
+          </div>
+        </div>
+        <div className="product">
+          <img src="" alt="" className="product-img" />
+          <div className="product-info">
+            <p className="product-name">Black coffee</p>
+            <p className="product-price">3$</p>
+            <button type="button">Add to cart</button>
+          </div>
+        </div>
+        <div className="product">
+          <img src="" alt="" className="product-img" />
+          <div className="product-info">
+            <p className="product-name">Black coffee</p>
+            <p className="product-price">3$</p>
+            <button type="button">Add to cart</button>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
 }
