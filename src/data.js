feb709d5c8b169a508f1f47ac0d3edca5e02f788
Shiielty <56679623+Shiielty@users.noreply.github.com>
2023-06-05 21:51:22 +0700

    Create coffee data, create Card component

---
+++ src/data.js
@@ -0,0 +1,74 @@
+const data = [
+  {
+    id: 1,
+    name: "Espresso",
+    price: 2.5,
+    amount: 0,
+    imgUrl: "",
+  },
+  {
+    id: 2,
+    name: "Cappuccino",
+    price: 3.0,
+    amount: 0,
+    imgUrl: "",
+  },
+  {
+    id: 3,
+    name: "Latte",
+    price: 3.5,
+    amount: 0,
+    imgUrl: "",
+  },
+  {
+    id: 4,
+    name: "Mocha",
+    price: 4.0,
+    amount: 0,
+    imgUrl: "",
+  },
+  {
+    id: 5,
+    name: "Americano",
+    price: 2.75,
+    amount: 0,
+    imgUrl: "",
+  },
+  {
+    id: 6,
+    name: "Macchiato",
+    price: 3.25,
+    amount: 0,
+    imgUrl: "",
+  },
+  {
+    id: 7,
+    name: "Flat White",
+    price: 3.75,
+    amount: 0,
+    imgUrl: "",
+  },
+  {
+    id: 8,
+    name: "Affogato",
+    price: 4.5,
+    amount: 0,
+    imgUrl: "",
+  },
+  {
+    id: 9,
+    name: "Drip Coffee",
+    price: 2.0,
+    amount: 0,
+    imgUrl: "",
+  },
+  {
+    id: 10,
+    name: "Iced Coffee",
+    price: 3.25,
+    amount: 0,
+    imgUrl: "",
+  },
+];
+
+export default data;
