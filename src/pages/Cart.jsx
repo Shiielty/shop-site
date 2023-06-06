1958a0de6334a39014c3421b6e1ad8343e66c7c9
Shiielty <56679623+Shiielty@users.noreply.github.com>
2023-06-06 23:47:59 +0700

    Refactor the cart component

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -26,53 +26,53 @@ export default function Cart({ coffeeData }) {
       <section className="cart">
         <h2>Shopping Cart</h2>
         <div className="cart-container">
-          {coffeeData
-            .filter((coffee) => amountCart[coffee.id].amount > 0)
-            .map((coffee) => (
-              <div className="cart-item">
-                <img src={coffee.imgUrl} alt="" srcset="" />
-                <div className="cart-item-info">
-                  <p>{coffee.name}</p>
-                  <p>{coffee.price * amountCart[coffee.id].amount}</p>
-                </div>
-                <div className="cart-item-counter">
-                  <button
-                    type="button"
-                    className="add-btn"
-                    onClick={() => handleRemove(coffee.id)}
-                  >
-                    <i class="fa-solid fa-minus"></i>
-                  </button>
-                  <span>{amountCart[coffee.id].amount}</span>
-                  <button
-                    type="button"
-                    className="remove-btn"
-                    onClick={() => handleAdd(coffee.id)}
-                  >
-                    <i class="fa-solid fa-plus"></i>
-                  </button>
-                </div>
-              </div>
-            ))}
+          {coffeeData.some((coffee) => amountCart[coffee.id].amount > 0)
+            ? coffeeData
+                .filter((coffee) => amountCart[coffee.id].amount > 0)
+                .map((coffee) => (
+                  <div className="cart-item">
+                    <img src={coffee.imgUrl} alt="" srcset="" />
+                    <div className="cart-item-info">
+                      <p>{coffee.name}</p>
+                      <p>{coffee.price * amountCart[coffee.id].amount}</p>
+                    </div>
+                    <div className="cart-item-counter">
+                      <button
+                        type="button"
+                        className="add-btn"
+                        onClick={() => handleRemove(coffee.id)}
+                      >
+                        <i class="fa-solid fa-minus"></i>
+                      </button>
+                      <span>{amountCart[coffee.id].amount}</span>
+                      <button
+                        type="button"
+                        className="remove-btn"
+                        onClick={() => handleAdd(coffee.id)}
+                      >
+                        <i class="fa-solid fa-plus"></i>
+                      </button>
+                    </div>
+                  </div>
+                ))
+            : null}
         </div>
-        {amountCart.some((item) => item.amount > 0) ? (
-          <div>
-            <p>
-              Total:{" "}
-              <span>
-                {coffeeData.reduce((sum, nextItem) => {
-                  return sum + nextItem.price * amountCart[nextItem.id].amount;
-                }, 0)}{" "}
-              </span>
-            </p>
-            <button type="button" className="checkout-btn">
-              Checkout
-            </button>
-            <button type="button" className="close-btn">
-              Close
-            </button>
-          </div>
-        ) : null}
+        <div>
+          <p>
+            Total:{" "}
+            <span>
+              {coffeeData.reduce((sum, nextItem) => {
+                return sum + nextItem.price * amountCart[nextItem.id].amount;
+              }, 0)}{" "}
+            </span>
+          </p>
+          <button type="button" className="checkout-btn">
+            Checkout
+          </button>
+          <button type="button" className="close-btn">
+            Close
+          </button>
+        </div>
       </section>
     </>
   );
